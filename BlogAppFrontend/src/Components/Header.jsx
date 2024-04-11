1d1f41bc19669496c2bc73078812f6a8ada763c8
psychic-coder <rohitganguly450@gmail.com>
2024-04-11 19:14:49 +0530

    server setup1

--- BlogAppFrontend/src/Components/Header.jsx
+++ BlogAppFrontend/src/Components/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" pill>
+          <Button gradientDuoTone="purpleToBlue" outline  pill>
             Sign-in
           </Button>
         </Link>
