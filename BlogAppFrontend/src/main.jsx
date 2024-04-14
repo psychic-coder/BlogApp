bdc366041f6f7901700494cc9e27ca14d36be4e6
psychic-coder <rohitganguly450@gmail.com>
2024-04-14 12:46:32 +0530

    redux Persist

--- BlogAppFrontend/src/main.jsx
+++ BlogAppFrontend/src/main.jsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import {store} from '../src/redux/store.js'
+import {store,persistor} from '../src/redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+//THE PERSISITGATE IS USED TO COVER EVERYTHING
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <PersistGate persistor={persistor}>
     <Provider store={store}>
     <App />
-  </Provider>,
-)
+  </Provider>
+  </PersistGate>
+);
--- BlogAppFrontend/src/redux/store.js
+++ BlogAppFrontend/src/redux/store.js
@@ -1,9 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore,combineReducers  } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
 //in the above line we are importing the userSlice.reducer
 
-export const store = configureStore({
-  reducer: {
-    user:userReducer,
-  },
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+
+//if we have multiple reducers then we have the power to combine the reducers
+const rootReducer=combineReducers({
+  user:userReducer,
 })
+ const persistConfig={
+  key:'root',
+  storage,
+  version:1,
+
+ };
+
+const persistedReducer=persistReducer(persistConfig,rootReducer);
+
+
+//inplace of adding all the reducers we just added the persistedReducer
+export const store=configureStore({
+  reducer:persistedReducer,
+  //the middleware is needed to prevent the error using redux toolkit
+  middleware:(getDefaultMiddleware)=>
+  getDefaultMiddleware({ serializableCheck:false}),
+});
+
+export const persistor=persistStore(store);
+
