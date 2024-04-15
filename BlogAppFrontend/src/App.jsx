ae0c4b0010f3e8dd5ca96b70650ffd5290ff2bb4
psychic-coder <rohitganguly450@gmail.com>
2024-04-15 15:33:23 +0530

    make dashboard private

--- BlogAppFrontend/src/App.jsx
+++ BlogAppFrontend/src/App.jsx
@@ -8,6 +8,7 @@ import DashBoard from "./pages/DashBoard";
 import Projects from "./pages/Projects";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import PrivateRoute from "./Components/PrivateRoute";
 
 function App() {
   return (
@@ -18,7 +19,10 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<SignUp />} />
+        {/*we are making the dashboard private so that the people who are signed in can only access it*/}
+        <Route element={<PrivateRoute/>}>
         <Route path="/dashboard" element={<DashBoard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
      <Footer/>
---
+++ BlogAppFrontend/src/Components/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Outlet,Navigate } from 'react-router-dom'
+
+
+//navigate is  a componenet
+//use Navigate is a hook
+
+function PrivateRoute() {
+    const {currentUser}=useSelector((state)=>state.user)
+  return (
+    currentUser? <Outlet/>:<Navigate to='/sign-in' />
+  )
+}
+
+export default PrivateRoute
