b5136caf374b96351f820ade0b027f3f483c215a
psychic-coder <rohitganguly450@gmail.com>
2024-04-17 19:02:46 +0530

    create get post api route

--- Backend/api/controllers/post.controller.js
+++ Backend/api/controllers/post.controller.js
@@ -1,25 +1,79 @@
 import Post from "../models/postModel.js";
-import errorHandler from '../utils/error.js';
+import errorHandler from "../utils/error.js";
 
-export const create=async (req,res,next)=>{
-    //console.log(req.user)
-    //we are  writing user inplace of body as we are checking the cookie in place of the body section
-    if(!req.user.isAdmin){
-        return next(errorHandler(403,'You are not allowed to create a post'))
-    }
-    if(!req.body.title || !req.body.content){
-        return next(errorHandler(400,'Please provide all the required fields'))
-    }
-    const slug= req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9]/g,'');
+export const create = async (req, res, next) => {
+  //console.log(req.user)
+  //we are  writing user inplace of body as we are checking the cookie in place of the body section
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all the required fields"));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, "");
 
-    const newPost=new Post({
-        ...req.body,slug,userId:req.user.id
-    });
-    try {
-        const savedPost=await newPost.save();
-        res.status(201).json(savedPost);
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
+export const getposts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    //when its 1 mongodb is gonna sort it ascending but if the order is -1 then mongodb in gonna sort it in reverse order
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        //using 'or' we can search in between two items
+        //'options '-- i, means lower case and upper case is not important
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({
+        updatedAt: sortDirection,
+      })
+      .skip(startIndex)
+      .limit(limit);
 
-    } catch (error) {
-        next(error)
-    }
-}
+      const totalPosts=await Post.countDocuments();
+      const now=new Date();
+      const oneMonthAgo=new Date(
+        now.getFullYear(),
+        now.getMonth()-1,
+        now.getDate()
+      );
+
+        //GTE stands for "greater then" 
+        const lastMonthPosts=await Post.countDocuments({
+            createdAt: {$gte:oneMonthAgo},
+        });
+
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMonthPosts,
+        });
+
+  } catch (error) {
+    next(error);
+  }
+};
