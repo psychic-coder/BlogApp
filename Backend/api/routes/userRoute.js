0a4f2673af236d8f59edc69497e2ca1b21412139
psychic-coder <rohitganguly450@gmail.com>
2024-04-20 13:15:37 +0530

    show comments of a post

--- Backend/api/routes/userRoute.js
+++ Backend/api/routes/userRoute.js
@@ -1,18 +1,23 @@
-import  express from  'express';
-import {deleteUser, signout, test, updateUser,getUsers} from '../controllers/user.controller.js'
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import {
+  deleteUser,
+  signout,
+  test,
+  updateUser,
+  getUsers,
+  getUser,
+} from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
-const router=express.Router();
+const router = express.Router();
 
-router.get('/test',test
-);
+router.get("/test", test);
 
-router.put('/update/:userId',verifyToken,updateUser);
-router.delete('/delete/:userId', verifyToken, deleteUser);
-router.post('/signout',signout);
-router.get('/getusers',verifyToken,getUsers)
-
-
+router.put("/update/:userId", verifyToken, updateUser);
+router.delete("/delete/:userId", verifyToken, deleteUser);
+router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 //we also have to import the user.controller in app.js
 export default router;
