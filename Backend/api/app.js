fce9af0a6836dd101a60a8da048df05395f2ae9a
psychic-coder <rohitganguly450@gmail.com>
2024-05-04 18:51:30 +0530

    port error

--- Backend/api/app.js
+++ Backend/api/app.js
@@ -52,7 +52,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
