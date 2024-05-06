c6ade51d35e563adae1456bacf1c670a21cf1cec
psychic-coder <rohitganguly450@gmail.com>
2024-05-06 10:25:34 +0530

    night mode error

--- Backend/BlogAppFrontend/src/Components/Header.jsx
+++ Backend/BlogAppFrontend/src/Components/Header.jsx
@@ -97,7 +97,7 @@ function Header() {
       </Button>
       <div className=" flex gap-2 md:order-2">
         <Button
-          className="w-12,h-10 hidden sm:inline"
+          className="w-12,h-10 inline"
           color="gray"
           onClick={() => {
             dispatch(toggleTheme());
